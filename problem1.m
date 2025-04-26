21469d0ae834644961b4f0bc076a8b7377b1bc25
Wenqian Weng <136841453+wwqqq16@users.noreply.github.com>
2025-04-25 21:46:14 -0400

    Update problem1.m

--- problem1.m
+++ problem1.m
@@ -1,8 +1,7 @@
 % Problem 1: Infinite-Horizon LQR Design (Corrected Version)
 
-% --------------------------------------------------
 % Step 1: Define the CORRECT system matrices A and B
-% --------------------------------------------------
+
 A = [0 1 0;
      0 0 1;
      10 -2 1];  % Corrected system dynamics matrix
